2732f9fc963adbef53b38e2cf03312f73f3c5a30
Divya Siras <divyashiras976@gmail.com>
2026-06-12 11:34:10 +0530

    Add files via upload
    A simple Python program that accepts two numbers from the user and displays the larger number using conditional statements.

---
+++ maximum-number.py
@@ -0,0 +1,14 @@
+
+def main():
+    print("Enter First No :")
+    no1 = int(input())
+
+    print("Enter Second No :")
+    no2 = int(input())
+
+    if (no1 > no2):
+        print("MAximum is :",no1)
+    else:
+        print("MAximum is :",no2)
+
+main()
